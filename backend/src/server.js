8b276424cae39f93f17bf46cea1ce2ea1a8f755e
Coll0 <mwandikwacollins@gmail.com>
2026-02-17 23:48:32 +0300

    Set up dependencies for the rate limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connect } from 'mongoose';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
   //console.log(`Request method is: ${req.method}, Request URL is: ${req.url}`);
   //next();
 //});
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
