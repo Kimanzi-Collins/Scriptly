e01a46cf827076325721329167c7ce4ba43f2b0a
Coll0 <mwandikwacollins@gmail.com>
2026-02-22 21:01:13 +0300

    feat: implement RateLimitedUI component and integrate it into Homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
+import { useState } from 'react'
 
 const Homepage = () => {
+  const {isRateLimited, setIsRateLimited} = useState(true);
+  const {notes, setNotes} = useState([]);
+  const {isLoading, setIsLoading} = useState(false);
+
   return (
     <div className='min-h-screen'>
       <Navbar />
-    </div>
+
+      {isRateLimited && <RateLimitedUI />}
+    </div>  
   )
 }
 
