75dc041cb584a92f76902701bf27a2b1575b71ae
Coll0 <mwandikwacollins@gmail.com>
2026-04-10 23:08:01 +0300

    feat: add unescape utility and NoteCard component
    - Introduced `unescape` function in the minimatch package to handle string un-escaping with options for Windows paths and magical braces.
    - Added TypeScript definitions and source maps for the `unescape` function.
    - Created a new `NoteCard` component in the frontend to display individual notes with a link to their details.

---
+++ frontend/src/components/NoteCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const NoteCard = ({ note }) => {
+  return (
+    <Link to={`/notes/${note._id}`} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-primary'>
+      <div className='card-body'>
+        <h3 className='card-title text-base-content'>{note.title}</h3>
+        <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
+        <div className></div>
+      </div>
+    </Link>
+  )
+}
+
+export default NoteCard
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { ArrowLeftIcon } from 'lucide-react'
 
 const Createpage = () => {
+  const[title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSubmit =  () => {
+
+  }
   return (
-    <div>Createpage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container-mx-auto px-4 py-5'>
+        <div className='max-w-2xl mx-auto'>
+          <Link to={'/'} className='btn btn-ghost mb-6'>
+          <ArrowLeftIcon className='size-5'/>
+          Back to Notes
+          </Link>
+          <div className='card bg-base-100'>
+            <div className='card-body'>
+              <h2 className='card-title text-2xl mb-4'>Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className='form-control mb-4'>
+                  <label className='label'>
+                    <span className='label-text'>Title</span>
+                  </label>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
